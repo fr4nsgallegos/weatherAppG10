20c0778be4010e66765cfdf230ed15b7c404ddc6
Jhonny Frans Gallegos Mendoza <jgallegos@evolutionsoluciones.com>
2024-12-07 09:27:08 -0500

    first interface

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -3,6 +3,26 @@ import 'package:flutter/material.dart';
 class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      backgroundColor: Color(0xff2C2F31),
+      appBar: AppBar(
+        title: Text(
+          "WeatherApp",
+          style: TextStyle(color: Colors.white),
+        ),
+        centerTitle: true,
+        backgroundColor: Color(0xff2C2F31),
+        actions: [
+          IconButton(
+            onPressed: () {},
+            icon: Icon(Icons.location_on),
+            color: Colors.white,
+          ),
+        ],
+      ),
+      body: Column(
+        children: [],
+      ),
+    );
   }
 }
